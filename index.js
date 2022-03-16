db7ac13e5023d0dc3f29f127242280c930888fe4
thiagopgc <thiagopereiragc@yahoo.com.br>
2022-03-16 15:55:39 -0300

    redirecionado a rota get usuario para pasta controller.

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express();
 const porta = 3000;
 app.use(express.json());
 
-app.get("/usuario", (req, res) => {
-  res.send("Rota get para usuário.");
-});
-
 app.listen(porta, () => {
   console.log(`Servidor aberto na porta ${porta}.`);
 });
