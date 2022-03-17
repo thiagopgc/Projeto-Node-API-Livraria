a1330b4443eb832fc73b966b8b7adf59701179f8
thiagopgc <thiagopereiragc@yahoo.com.br>
2022-03-17 16:25:09 -0300

    criando as queries na DAO para passar nas rotas CRUD.

--- src/Controller/controller-usuario.js
+++ src/Controller/controller-usuario.js
@@ -14,6 +14,53 @@ const usuarioLivraria = (app, bd) => {
         res.json(erro);
       });
   });
+
+  app.get("/usuario/email/:email", (req, res) => {
+    const email = req.params.email;
+    usuarioDAO
+      .filtrarUsuario(email)
+      .then((resposta) => {
+        res.json(resposta);
+      })
+      .catch((erro) => {
+        res.json(erro);
+      });
+  });
+
+  app.post("/usuario", (req, res) => {
+    // Recebe o corpo da requisição
+    const body = req.body;
+
+    // Como temos validações na nossa model, usamos o try/catch
+    // para pegar esse erro e enviar como mensagem para nosso cliente
+    try {
+      // cria uma instancia de Usuario com validação dos dados
+      // apartir do corpo que foi recebido
+      const novoUsuario = new UsuarioModel(
+        body.nome,
+        body.email,
+        body.telefone,
+        body.endereco,
+        body.senha
+      );
+
+      // insere a instância do usuario no banco de dados
+      usuarioDAO
+        .insereUsuario(novoUsuario)
+        .then((resposta) => {
+          res.json(resposta);
+        })
+        .catch((erro) => {
+          res.json(erro);
+        });
+    } catch (error) {
+      // Envia o erro, caso exista
+      res.json({
+        msg: error.message,
+        erro: true,
+      });
+    }
+  });
 };
 
 export default usuarioLivraria;
--- src/DAO/usuarioDAO.js
+++ src/DAO/usuarioDAO.js
@@ -20,6 +20,59 @@ class UsuarioDAO {
       });
     });
   };
+
+  filtrarUsuario = (email) => {
+    return new Promise((resolve, reject) => {
+      this.db.all(
+        "SELECT * FROM USUARIOS WHERE EMAIL = 'thiagopereira@yahoo.com'",
+        email,
+        (error, rows) => {
+          if (error) {
+            reject({
+              mensagem: error.message,
+              erro: true,
+            });
+          } else {
+            resolve({
+              usuario: rows,
+              erro: false,
+            });
+          }
+        }
+      );
+    });
+  };
+  inserirUsuario = (novoUsuario) => {
+    return new Promise((resolve, reject) => {
+      // Query com ? para evitar SQL Injection
+      // NUNCA DEVEMOS USAR COM TEMPLATE STRING
+      // Nós inserimos os dados a serem substituidos depois da query
+      // Ou separado por vírgula (QUERY, a,b,c, callback)
+      // Ou em um array (QUERY, [a,b,c] , callback)
+      this.db.run(
+        "INSERT INTO USUARIOS(NOME, EMAIL, TELEFONE, ENDERECO, SENHA) VALUES (?, ?, ?, ?, ?)",
+        novoUsuario.nome,
+        novoUsuario.email,
+        novoUsuario.telefone,
+        novoUsuario.endereco,
+        novoUsuario.senha,
+        (error) => {
+          if (error) {
+            reject({
+              mensagem: error.message,
+              erro: true,
+            });
+          } else {
+            resolve({
+              mensagem: `Usuário ${novoUsuario.nome} inserido com sucesso`,
+              usuario: novoUsuario,
+              erro: false,
+            });
+          }
+        }
+      );
+    });
+  };
 }
 
 export default UsuarioDAO;
